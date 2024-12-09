d833e8f3201ed14796b9b7f8ab7fb58e0fbe3e72
wizardbyron <1400427+wizardbyron@users.noreply.github.com>
2024-12-10 00:38:57 +0800

    重新调整目录结构

--- points.py
+++ points.py
@@ -2,12 +2,74 @@ from datetime import datetime
 
 import akshare as ak
 import pandas as pd
-from utils.pivot_points import classic
+
+
+def classic(high: float, low: float, close: float) -> dict[str, float]:
+    """
+    计算经典支撑位和阻力位
+    :param high: 最高价
+    :param low: 最低价
+    :param close: 收盘价
+    :return: 经典支撑位和阻力位
+    """
+    p = (high + low + close)/3
+
+    s1 = 2 * p - high
+    s2 = p - (high - low)
+    s3 = low - 2 * (high - p)
+
+    r1 = 2 * p - low
+    r2 = p + (high - low)
+    r3 = high + 2 * (p - low)
+
+    return {
+        "阻力位3": r3,
+        "阻力位2": r2,
+        "阻力位1": r1,
+        "枢轴点": p,
+        "支撑位1": s1,
+        "支撑位2": s2,
+        "支撑位3": s3,
+        "预期波动率1": (r1-s1)/p * 100,
+        "预期波动率2": (r2-s2)/p * 100,
+        "预期波动率3": (r3-s3)/p * 100
+    }
+
+
+def fibonacci(high: float, low: float, close: float) -> dict[str, float]:
+    """
+    计算斐波那契支撑位和阻力位
+    :param high: 最高价
+    :param low: 最低价
+    :param close: 收盘价
+    :return: 斐波那契支撑位和阻力位
+    """
+    p = (high + low + close)/3
+    s1 = p - (high - low) * 0.382
+    s2 = p - (high - low) * 0.618
+    s3 = p - (high - low)
+    r1 = p + (high - low) * 0.382
+    r2 = p + (high - low) * 0.618
+    r3 = p + (high - low)
+
+    return {
+        "阻力位3": r3,
+        "阻力位2": r2,
+        "阻力位1": r1,
+        "枢轴点": p,
+        "支撑位1": s1,
+        "支撑位2": s2,
+        "支撑位3": s3,
+        "预期波动率1": (r1-s1)/p * 100,
+        "预期波动率2": (r2-s2)/p * 100,
+        "预期波动率3": (r3-s3)/p * 100
+    }
+
 
 level = 5
 today = datetime.today()
 today_str = today.strftime('%Y%m%d')
-offset_days = today.weekday()+1
+
 us_symbol_dict = ak.stock_us_spot_em()
 
 df_input = pd.read_csv('selected.csv', dtype={'代码': str})
@@ -28,7 +90,14 @@ for index, row in df_input.iterrows():
     else:
         continue
 
-    df = history_klines[- offset_days - level: - offset_days]
+    # 获取上周的交易数据
+    if today.weekday() < 5:  # 交易日
+        off_day = today.weekday()
+        if history_klines['日期'].iloc[-1] == today.strftime('%Y-%m-%d'):
+            off_day += 1  # 当天收盘后，去掉当天数据
+        df = history_klines[-level-off_day:-off_day]
+    else:  # 周末
+        df = history_klines[-level:]
 
     print(df)
 
