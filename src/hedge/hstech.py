36d82b445a11b28b3f43c3a9620e05944565ae55
wizardbyron <1400427+wizardbyron@users.noreply.github.com>
2025-03-25 23:12:01 +0800

    增加对冲监控

---
+++ src/hedge/hstech.py
@@ -0,0 +1,50 @@
+
+import datetime
+
+from time import sleep
+from zoneinfo import ZoneInfo
+
+import akshare as ak
+
+from src.util.indicators import pivot_points
+
+symbols = ['07226', '07552']
+periods = ['daily', 'weekly']
+
+
+def is_in_trading_time():
+    # 定义开始时间和结束时间
+    now = datetime.datetime.now(ZoneInfo('America/New_York'))
+
+    # 判断当前时间是否在指定范围内
+    return now.hour in range(9, 16)
+
+
+def generate_points(klines):
+    points = pivot_points(klines[-2:-1])
+    today = klines.iloc[-1]
+    vwap = today["成交额"].sum() / today["成交量"].sum()
+    points.loc["*VWAP"] = vwap
+    points.loc["*最高"] = today["最高"]
+    points.loc["*开盘"] = today["开盘"]
+    points.loc["*最低"] = today["最低"]
+    points.loc["*当前>"] = today["收盘"]
+    # p = points.loc["枢轴点", "中间值"]
+    latest = today["收盘"]
+    points["波动率"] = (points["中间值"] - latest)/latest
+    points["波动率"] = points["波动率"].map(lambda x: '{:.2%}'.format(x))
+    points = points.sort_values(by="中间值", ascending=False)
+    points = points[["中间值", "波动率"]]
+    points = points.round(3)
+    return points
+
+
+while (is_in_trading_time()):
+    for symbol in symbols:
+        for period in periods:
+            klines = ak.stock_hk_hist(
+                symbol=symbol,
+                period=period)
+            points = generate_points(klines)
+            print(f"{symbol}-{period}\n{points}\n")
+    sleep(60)
