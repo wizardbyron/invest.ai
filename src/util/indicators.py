b0fffdc6b8b1c1313313049115636a0f8132fa67
wizardbyron <1400427+wizardbyron@users.noreply.github.com>
2025-03-28 00:21:11 +0800

    更新枢轴点

--- src/util/indicators.py
+++ src/util/indicators.py
@@ -82,16 +82,12 @@ def pivot_points(df_input: DataFrame) -> DataFrame:
     row_index = c_points.keys()
     df_output = pd.DataFrame(item, index=row_index)
     df_output["中间值"] = (df_output["经典"] + df_output["斐波那契"])/2
-
-    p = c_points["枢轴点"]
-    df_output["中间值波动率"] = (df_output["中间值"] - p)/p
-    df_output["中间值波动率"] = df_output["中间值波动率"].map(lambda x: '{:.2%}'.format(x))
-
     df_output = df_output.round(3)
+
     return df_output
 
 
-def merge_points(klines: DataFrame) -> DataFrame:
+def merge_points(klines: DataFrame, type: str = "中间值") -> DataFrame:
     points = pivot_points(klines[-2:-1])
     points.loc["*昨收"] = klines.iloc[-2]["收盘"]
     today = klines.iloc[-1]
@@ -100,9 +96,9 @@ def merge_points(klines: DataFrame) -> DataFrame:
     points.loc["*最低"] = today["最低"]
     points.loc["*当前>"] = today["收盘"]
     latest = today["收盘"]
-    points["波动率"] = (points["中间值"] - latest)/latest
-    points["波动率"] = points["波动率"].map(lambda x: '{:.2%}'.format(x))
-    points = points.sort_values(by="中间值", ascending=False)
-    points = points[["中间值", "波动率"]]
+    points["波动率"] = (points[type] - latest)/latest
+    points["波动率"] = points[type].map(lambda x: '{:.2%}'.format(x))
+    points = points.sort_values(by=type, ascending=False)
+    points = points[[type, "波动率"]]
     points = points.round(3)
     return points
