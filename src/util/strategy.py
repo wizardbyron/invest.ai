d438157647512e8a84972c27e9853f4c40560f22
wizardbyron <1400427+wizardbyron@users.noreply.github.com>
2025-04-01 07:59:55 +0800

    增加周线枢轴点策略

--- src/util/strategy.py
+++ src/util/strategy.py
@@ -18,3 +18,16 @@ def intraday(points: DataFrame):
     else:
         print(f"当前价格{cur_price}在{buy_price}和{sell_price}之间，观望")
     print("="*40)
+
+
+def weekly(points: DataFrame):
+    cur_price = points.loc["*当前>", "中间值"]
+    buy_price = points.loc["支撑位2.0", "中间值"]
+    sell_price = points.loc["阻力位2.0", "中间值"]
+    if cur_price > sell_price:
+        print(f"当前价格{cur_price}高于{sell_price}，建议卖出")
+    elif cur_price < buy_price:
+        print(f"当前价格{cur_price}低于{buy_price}，建议买入")
+    else:
+        print(f"当前价格{cur_price}在{buy_price}和{sell_price}之间，观望")
+    print("="*40)
