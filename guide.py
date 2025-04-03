3b72c294ad564cea8bf012b8b3fa1d6816c9daab
wizardbyron <1400427+wizardbyron@users.noreply.github.com>
2025-04-03 22:41:37 +0800

    采用枢轴点网格交易策略

--- guide.py
+++ guide.py
@@ -6,56 +6,44 @@ import time
 import akshare as ak
 
 from src.util.indicators import merge_points
-from src.util.strategy import intraday, weekly
+from src.util.strategy import pivot_points_grid
 
 
-class TradingGuide:
-    def watch(self, market: str, symbol: str):
-        """监控价格
+def guide(market: str, symbol: str):
+    """获取价格交易指南
 
-        Args:
-            market (str):  a, etf, hk
-            symbol (str): 代码
-        """
-        while (True):
-            self.guide(market, symbol)
-            time.sleep(10)
+    Args:
+        market (str): a, etf, hk
+        symbol (str): 代码
 
-    def guide(self, market: str, symbol: str):
-        """获取价格交易指南
+    Raises:
+        ValueError: _description_
+    """
 
-        Args:
-            market (str): a, etf, hk
-            symbol (str): 代码
+    for period in ['daily', 'weekly']:
+        if market == "etf":
+            klines = ak.fund_etf_hist_em(
+                symbol=symbol,
+                period=period)
+        elif market == "hk":
+            klines = ak.stock_hk_hist(
+                symbol=symbol,
+                period=period)
+        elif market == "a":
+            klines = ak.stock_zh_a_hist(
+                symbol=symbol,
+                period=period)
+        else:
+            raise ValueError("Invalid market type")
 
-        Raises:
-            ValueError: _description_
-        """
-
-        for period in ['daily', 'weekly']:
-            if market == "etf":
-                klines = ak.fund_etf_hist_em(
-                    symbol=symbol,
-                    period=period)
-            elif market == "hk":
-                klines = ak.stock_hk_hist(
-                    symbol=symbol,
-                    period=period)
-            elif market == "a":
-                klines = ak.stock_zh_a_hist(
-                    symbol=symbol,
-                    period=period)
-            else:
-                raise ValueError("Invalid market type")
-
-            points = merge_points(klines)
-            print(f"{symbol}-{period}\n{klines[-1:]}\n{points}\n")
-            weekly(points)
+        points = merge_points(klines)
+        print(f"{symbol}-{period}\n{klines[-1:]}\n{points}\n")
+        pivot_points_grid(points)
 
 
 if __name__ == "__main__":
     start_time = time.time()
-    fire.Fire(TradingGuide)
+    fire.Fire(guide)
     end_time = time.time()
     duration = end_time - start_time
     print(f"[运行时间：{duration:.2f}秒]")
--- src/util/strategy.py
+++ src/util/strategy.py
@@ -1,33 +1,16 @@
 from pandas import DataFrame
 
 
-def intraday(points: DataFrame):
+def pivot_points_grid(points: DataFrame, buy_point: float = 2.5, sell_point: float = 2.5):
     cur_price = points.loc["*当前>", "中间值"]
-    if points.loc["*开盘", "中间值"] > points.loc["*昨收", "中间值"]:  # 高开
-        buy_point = "支撑位0.5"
-        sell_point = "阻力位1.5"
-    else:  # 低开
-        buy_point = "支撑位1.5"
-        sell_point = "阻力位0.5"
-    buy_price = points.loc[buy_point, "中间值"]
-    sell_price = points.loc[sell_point, "中间值"]
+    buy_price = points.loc[f"支撑位{buy_point}", "中间值"]
+    sell_price = points.loc[f"阻力位{sell_point}", "中间值"]
     if cur_price > sell_price:
         print(f"当前价格{cur_price}高于{sell_price}，建议卖出")
+        return "SELL", cur_price
     elif cur_price < buy_price:
         print(f"当前价格{cur_price}低于{buy_price}，建议买入")
+        return "BUY", cur_price
     else:
         print(f"当前价格{cur_price}在{buy_price}和{sell_price}之间，观望")
-    print("="*40)
-
-
-def weekly(points: DataFrame):
-    cur_price = points.loc["*当前>", "中间值"]
-    buy_price = points.loc["支撑位2.0", "中间值"]
-    sell_price = points.loc["阻力位2.0", "中间值"]
-    if cur_price > sell_price:
-        print(f"当前价格{cur_price}高于{sell_price}，建议卖出")
-    elif cur_price < buy_price:
-        print(f"当前价格{cur_price}低于{buy_price}，建议买入")
-    else:
-        print(f"当前价格{cur_price}在{buy_price}和{sell_price}之间，观望")
-    print("="*40)
+        return "HOLD", cur_price
