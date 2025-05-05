2c803d2797ebd67cf706debdf890d2eac40001c2
wizardbyron <1400427+wizardbyron@users.noreply.github.com>
2025-05-06 07:49:42 +0800

    更新策略可选枢轴点

--- src/strategy.py
+++ src/strategy.py
@@ -1,7 +1,7 @@
 from pandas import DataFrame
 
 
-def pivot_points_grid(points: DataFrame, buy_point: float = 2, sell_point: float = 2):
+def pivot_points_grid(points: DataFrame, buy_point: float = 2, sell_point: float = 2, type="中间值"):
     """枢轴点网格策略
 
     Args:
@@ -12,9 +12,9 @@ def pivot_points_grid(points: DataFrame, buy_point: float = 2, sell_point: float
     Returns:
         _type_: _description_
     """
-    cur_price = points.loc["*当前>", "中间值"]
-    buy_price = points.loc[f"支撑位{buy_point}", "中间值"]
-    sell_price = points.loc[f"阻力位{sell_point}", "中间值"]
+    cur_price = points.loc["*当前>", type]
+    buy_price = points.loc[f"支撑位{buy_point}", type]
+    sell_price = points.loc[f"阻力位{sell_point}", type]
     if cur_price > sell_price:
         print(f"当前价格{cur_price}高于{sell_price}，建议卖出")
         return "SELL", cur_price
