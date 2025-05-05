2308b14a84529dff22d0944cef1837afd34f7fa0
wizardbyron <1400427+wizardbyron@users.noreply.github.com>
2025-05-06 00:11:09 +0800

    更新 webui

--- webui.py
+++ webui.py
@@ -12,40 +12,42 @@ def get_points(stock_code: str, period: str):
     df = merge_points(klines.iloc[-1], points)
     df.index.name = "参考点位"
     df = df.reset_index()
-    return df
+    return tzone,df
 
 
 daily_table=None
 weekly_table= None
-def show_stock_data():
+def update_tables():
     global daily_table
     global weekly_table
     stock_code = input_code.value
-    if len(stock_code)>=5:
-        df_daily = get_points(stock_code, "daily")
-        df_weekly = get_points(stock_code, "weekly")
-        
+    if stock_code.strip() != "" :
+        tzone,df_daily = get_points(stock_code, "daily")
+        tzone,df_weekly = get_points(stock_code, "weekly")
+        ui.notify(f"{stock_code}数据更新时间：{nowstr(tzone)}")
         with ui.row():
             if weekly_table is None or daily_table is None:
-                daily_table = ui.table.from_pandas(df_daily,title="今日枢轴点")
-                weekly_table = ui.table.from_pandas(df_weekly,title="本周枢轴点")
+                daily_table = ui.table.from_pandas(df_daily,title=f"日内交易参考")
+                weekly_table = ui.table.from_pandas(df_weekly,title="本周交易参考")
+
             else:
                 ui.table.update_from_pandas(daily_table, df_daily)
                 ui.table.update_from_pandas(weekly_table, df_weekly)
-                print("更新表格:"+nowstr())        
+
                 
 
 ui.page_title('Invest.AI')
 ui.dark_mode().enable()
-ui.timer(5.0, lambda: show_stock_data())
+ui.timer(10.0, lambda: update_tables())
 
 with ui.header() as header:
     ui.icon('add_chart').classes('text-3xl')
     ui.label('Invest.AI').classes('text-xl text-bold')
 
+
 with ui.row():
     input_code = ui.input(placeholder='股票代码').props('outlined dense')
-    ui.button('update', on_click=show_stock_data)
+    ui.button('update', on_click=update_tables)
     ui.separator()
 
 with ui.footer() as footer:
