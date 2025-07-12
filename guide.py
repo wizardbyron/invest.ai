43cf160309b5fde724b61ddc85bf56f7bcc2f476
wizardbyron <1400427+wizardbyron@users.noreply.github.com>
2025-07-12 22:03:34 +0800

    重构代码

--- guide.py
+++ guide.py
@@ -5,25 +5,13 @@ import time
 
 import pandas as pd
 
-from src.data import get_points
 from src.strategy import pivot_points_grid
 from src.util import send_voice, numbers_in_chinese, todaystr
 
 
-p_sell = {
-    "weekly": 2.0,
-    "daily": 1.5
-}
-
-p_buy = {
-    "weekly": 2.0,
-    "daily": 1.5
-}
-
-
 class Guide:
     @classmethod
-    def guide(cls, symbol: str, series: str = "中间值") -> None:
+    def single(cls, symbol: str, series: str = "中间值") -> None:
         """获取价格交易指南
 
         Args:
@@ -34,53 +22,23 @@ class Guide:
             ValueError: _description_
         """
         for period in ['weekly', 'daily']:
-            points = get_points(symbol, series, period)
-            pivot_points_grid(points, p_buy[period], p_sell[period], series)
+            pivot_points_grid(symbol, period, series)
 
     @classmethod
-    def watch(cls, symbol: str, series: str = "中间值") -> None:
-        """检测价格，发送交易通知
-
-        Args:
-            symbol (str): 代码
-            series (str, optional): 枢轴点类型：经典/斐波那契额/中间值. Defaults to "中间值".
-
-        Raises:
-            ValueError: _description_
-        """
-        is_in_trading_time = True
-        while is_in_trading_time:
-            for period in ['weekly', 'daily']:
-                points = get_points(symbol, series, period)
-                order, price = pivot_points_grid(
-                    points, p_buy[period], p_sell[period], series)
-                if order != "持有":
-                    msg = f"注意 {price} 元 {order} 股票 {numbers_in_chinese(str(symbol))}"
-                    send_voice(msg)
-            time.sleep(10)
-
-    @classmethod
-    def portfolio_guide(cls, file: str = "./input/portfolio.csv", series: str = "中间值"):
+    def portfolio(cls, file: str = "./input/portfolio.csv", series: str = "中间值"):
         """_summary_
 
         Args:
             data (_type_): _description_
         """
 
-        df_portfolio = pd.read_csv(file, dtype={"代码": str})
-        for period in ['weekly', 'daily']:
-            file_name = f"./output/guides/{todaystr()}_{period}.xlsx"
-            with pd.ExcelWriter(file_name, engine='openpyxl') as writer:
-                for symbol, name in df_portfolio[["代码", "名称"]].values:
-                    print(f'{period}-{symbol}')
-                    points = get_points(symbol, series, period)
-                    order, price = pivot_points_grid(
-                        points,
-                        p_buy[period],
-                        p_sell[period],
-                        series)
-                    sheet_name = f'{symbol}-{name}'
-                    points.to_excel(writer, sheet_name=sheet_name)
+        df_portfolio = pd.read_csv(file, dtype={"代码": str, "名称": str})
+        for symbol, name in df_portfolio[["代码", "名称"]].values:
+            for period in ['weekly', 'daily']:
+                order, price = pivot_points_grid(symbol, period, series)
+                if order != "持有":
+                    msg = f"注意 {price} 元 {order} 股票 {numbers_in_chinese(name)}"
+                    print(f'{period}-{symbol}-{name}:{msg}')
 
 
 if __name__ == "__main__":
--- src/strategy.py
+++ src/strategy.py
@@ -1,7 +1,48 @@
 from pandas import DataFrame
 
+from src.data import history_klines
+from src.indicators import pivot_points_table, merge_points
+from src.util import in_trading_time, is_weekend
 
-def pivot_points_grid(points: DataFrame, buy_point: float, sell_point: float, series="中间值"):
+sell_point = {
+    "weekly": 2.0,
+    "daily": 1.5
+}
+
+buy_point = {
+    "weekly": 2.0,
+    "daily": 1.5
+}
+
+
+def get_points(symbol: str, period: str, series: str = "中间值") -> DataFrame:
+    """将枢轴点和当前的价格进行合并
+
+    Args:
+        symbol (str): 股票代码
+        series (str): "经典"/"斐波那契"/"中间值", defaults to "中间值"
+        period (str): 周期，weekly or daily
+
+    Returns:
+        DataFrame: _description_
+    """
+    tzone, klines = history_klines(str(symbol), period)
+    if in_trading_time(tzone):
+        data = klines[-2:-1]
+    else:
+        if is_weekend() or period == "daily":
+            data = klines[-1:]
+        else:
+            data = klines[-2:-1]
+
+    points = merge_points(klines.iloc[-1],
+                          pivot_points_table(data),
+                          series)
+    print(f"\n{symbol}-{period}:\n{data}\n{points}")
+    return points
+
+
+def pivot_points_grid(symbol: str, period: str, series: str = "中间值"):
     """枢轴点网格策略
 
     Args:
@@ -12,15 +53,16 @@ def pivot_points_grid(points: DataFrame, buy_point: float, sell_point: float, se
     Returns:
         _type_: _description_
     """
+    points = get_points(symbol, series, period)
     cur_price = points.loc["*当前>", series]
-    buy_price = points.loc[f"支撑位{buy_point:.1f}", series]
-    sell_price = points.loc[f"阻力位{sell_point:.1f}", series]
+    buy_price = points.loc[f"支撑位{buy_point[period]:.1f}", series]
+    sell_price = points.loc[f"阻力位{sell_point[period]:.1f}", series]
     if cur_price > sell_price:
-        print(f"当前价格{cur_price}高于{sell_price}，建议卖出")
+        print(f"{symbol}当前价格{cur_price}高于{period}阻力价格{sell_price}，建议卖出")
         return "卖出", cur_price
     elif cur_price < buy_price:
-        print(f"当前价格{cur_price}低于{buy_price}，建议买入")
+        print(f"{symbol}当前价格{cur_price}低于{period}支撑价格{buy_price}，建议买入")
         return "买入", cur_price
     else:
-        print(f"当前价格{cur_price}在{buy_price}和{sell_price}之间，观望")
+        print(f"{symbol}当前价格{cur_price}在{period}支撑价格{buy_price}和阻力价格{sell_price}之间，观望")
         return "持有", cur_price
