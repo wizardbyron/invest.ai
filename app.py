3e993b24fbf4edec90cda3f2699da60bbd8e97a6
wizardbyron <1400427+wizardbyron@users.noreply.github.com>
2025-07-23 12:33:49 +0800

    重构app.py

--- app.py
+++ app.py
@@ -1,22 +1,28 @@
 import streamlit as st
 
 from src.strategy import pivot_points_grid
+from src.util import nowstr
 
-col_symbol, col_button = st.columns([3, 1],  vertical_alignment="bottom")
 
-symbol = col_symbol.text_input("Symbol")
+def pivot_df(st: st, symbol: str, period: str):
+    resp_weekly = pivot_points_grid(symbol, period)
+    if resp_weekly['order'] == '买入':
+        st.badge(f"{period}-买入", color="red")
+    elif resp_weekly['order'] == '卖出':
+        st.badge(f"{period}-卖出", color="green")
+    else:
+        st.badge(f"{period}-观望", color="blue")
+    st.dataframe(resp_weekly['merged_table'], height=700)
 
-if col_button.button('分析', use_container_width=True) and len(symbol) >= 3:
+
+symbol = st.text_input("请输入股票代码", max_chars=6)
+if st.button('分析', use_container_width=True) and len(symbol) >= 3:
     with st.status("Loading...", expanded=False) as status:
-        col_daily, col_weekly = st.columns([2, 2])
+        col_weekly, col_daily = st.columns([2, 2])
 
-        resp_daily = pivot_points_grid(symbol, "daily")
-        col_daily.caption("Daily")
-        col_daily.dataframe(resp_daily['merged_table'], height=700)
-        col_daily.caption(resp_daily['message'])
+        pivot_df(col_weekly, symbol, "weekly")
+        pivot_df(col_daily, symbol, "daily")
 
-        resp_weekly = pivot_points_grid(symbol, "weekly")
-        col_weekly.caption("Weekly")
-        col_weekly.dataframe(resp_weekly['merged_table'], height=700)
-        col_weekly.caption(resp_weekly['message'])
-        status.update(label="Done", state="complete", expanded=True)
+        status.update(label=f"{nowstr()}分析完毕，结果如下",
+                      state="complete",
+                      expanded=True)
