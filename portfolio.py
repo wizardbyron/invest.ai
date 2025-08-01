b0bd1f016c78f12354927aba39ee17d4c427f89b
wizardbyron <1400427+wizardbyron@users.noreply.github.com>
2025-08-01 23:42:37 +0800

    新增全天候投资组合

--- portfolio.py
+++ portfolio.py
@@ -101,7 +101,7 @@ class PortfolioBuilder:
                 output_md = append_discliamer(output_md)
                 output_md = remove_leading_spaces(output_md)
 
-                file_path = f"docs/投资组合/{type}投资组合_自选_{llm_service}.md"
+                file_path = f"docs/投资组合/{type}全天候投资组合_{llm_service}.md"
 
                 with open(file_path, "w") as f:
                     f.write(output_md)
@@ -229,6 +229,78 @@ class PortfolioBuilder:
         page = page[page.find("更新时间"):]
 
         print(self.__evaluate(doc[0].page_content))
+
+    def all_whethers(self, type: str = "A股ETF"):
+        """全天候投资组合
+
+        Args:
+            type (str, optional): 品种名称. Defaults to "A股ETF".
+        """
+
+        prompt = f"""请帮我构建一份{type}的全天候投资组合，要求如下：
+
+        - 请以表格的方式输出投资组合
+        - {type}的代码名称, 以及在投资组合内的占比
+        - 入选投资组合的原因
+        - 投资组合的交易频率或者再平衡周期建议
+        - 根据风险等级的低中高顺序输出
+
+        并用以下格式输出投资组合的交易策略：
+
+        ## 风险等级
+
+        ### {type} 的名称（{type}的交易代码）
+
+        - 在投资组合内的占比
+        - 入选投资组合的原因
+        - 具体的交易策略，并解释交易策略
+        - 交易频率
+        - 每个 {type} 的止盈和止损策略
+
+        ## 投资组合交易策略
+        """
+
+        prompt = remove_leading_spaces(prompt)
+
+        print(prompt)
+
+        messages = [
+            SystemMessage(
+                content="你是一个专业的投资人，可以根据我所给出的投资标的编制出投资组合，做到风险和收益兼顾。"
+            ),
+            HumanMessage(
+                content=prompt
+            )
+        ]
+
+        for llm_service, model in self.llm:
+            chat = create_chat(llm_service, model)
+
+            try:
+                print(f"模型: {model}")
+
+                response = chat.invoke(messages)
+
+                output_md = f"""# {type}全天候投资组合 - {llm_service}
+
+                更新时间: {nowstr()}
+
+                模型: {model}
+
+                ## 投资组合参考
+
+                {response.content}
+                """
+                output_md = append_discliamer(output_md)
+                output_md = remove_leading_spaces(output_md)
+
+                file_path = f"docs/投资组合/{type}全天候投资组合_{llm_service}.md"
+
+                with open(file_path, "w") as f:
+                    f.write(output_md)
+
+            except Exception as e:
+                print(f"调用模型[{model}]失败:\n{e}")
 
 
 if __name__ == "__main__":
