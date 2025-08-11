55626b8aac41908ed9c8be9ccefb58e5db1b22c4
wizardbyron <1400427+wizardbyron@users.noreply.github.com>
2025-08-11 19:15:45 +0800

    调整投资组合和构建脚本

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker build -t invest-ai:$(date +%Y%m%d) -f Dockerfile .
-docker image save -o invest-ai.$(date +%Y%m%d).tar invest-ai:$(date +%Y%m%d)
+docker image save -o output/invest-ai.$(date +%Y%m%d).tar invest-ai:$(date +%Y%m%d)
