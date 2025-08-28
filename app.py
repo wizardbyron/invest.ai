e86993ee0a1c03c0a21f06cd0634a47f8084c7a2
wizardbyron <1400427+wizardbyron@users.noreply.github.com>
2025-08-29 00:47:43 +0800

    更新页面设置

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 import streamlit as st
 
+st.set_page_config(page_title="Invest.AI - 量化投资助手")
 pages = {
     "Invest.AI": [
         st.Page("src/apps/individual.py",
                 title="个股交易参考",
                 icon=":material/explore:"),
         st.Page("src/apps/portfolio.py",
-                title="投资组合",
+                title="投资组合参考",
                 icon=":material/fact_check:"),
     ],
 }
