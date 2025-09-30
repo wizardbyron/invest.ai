a3fdf5053194d08e6146cd5844662ed987fbb854
wizardbyron <1400427+wizardbyron@users.noreply.github.com>
2025-09-30 23:27:29 +0800

    调整投资组合

--- src/apps/portfolio.py
+++ src/apps/portfolio.py
@@ -7,13 +7,10 @@ from src.strategy import pivot_points_grid
 from src.util import nowstr, format_for_markdown, disclaimer_md
 
 portfolios = {
+    '默认': 'all',
     '基础-5': 'basic_5',
     '基础-10': 'basic_10',
-    '调和-5': 'blended_5',
-    '调和-10': 'blended_10',
-    '躺平-5': 'layflat_5',
-    '躺平-10': 'layflat_10',
-    '默认': 'all',
+    '躺平': 'layflat',
 }
 
 option = st.selectbox("投资组合", portfolios.keys())
