9b20d162d3262b2387879747673c285c98185482
wizardbyron <1400427+wizardbyron@users.noreply.github.com>
2025-10-10 22:16:50 +0800

    增加AI交易指南

--- guide.py
+++ guide.py
@@ -9,85 +9,101 @@ from tabulate import tabulate
 from tqdm import tqdm
 
 from src.data import get_stock_name
-from src.strategy import pivot_points_grid
-from src.util import nowstr, format_for_term
+from src.strategy import pivot_points_grid, ai_guide
+from src.util import nowstr, todaystr, format_for_term
 
 
-def guide(portfolio: str = "all", series: str = "参考价") -> None:
-    """获取交易指南
+class Guide:
 
-    Args:
-        symbol (str): 代码 or ""
-        series (str, optional): 基准价类型：经典/斐波那契额/参考价. Defaults to "参考价".
+    @classmethod
+    def guide(clz, portfolio: str = "all", series: str = "参考价") -> None:
+        """获取交易指南
 
-    Raises:
-        ValueError: _description_
-    """
+        Args:
+            symbol (str): 代码 or ""
+            series (str, optional): 基准价类型：经典/斐波那契额/参考价. Defaults to "参考价".
 
-    try:
-        file = f'./input/portfolios/{portfolio}.csv'
-        df_portfolio = pd.read_csv(file, dtype={"代码": str, "名称": str})
-        symbols = df_portfolio[["代码", "名称"]].values
-    except FileNotFoundError:
-        symbols = [[str(portfolio).upper(), get_stock_name(str(portfolio))]]
+        Raises:
+            ValueError: _description_
+        """
 
-    output_dict = {
-        '代码': [],
-        '名称': [],
-        '当前价格': [],
-        '周建议': [],
-        '日建议': [],
-        '均价偏移': [],
+        try:
+            file = f'./input/portfolios/{portfolio}.csv'
+            df_portfolio = pd.read_csv(file, dtype={"代码": str, "名称": str})
+            symbols = df_portfolio[["代码", "名称"]].values
+        except FileNotFoundError:
+            symbols = [
+                [str(portfolio).upper(), get_stock_name(str(portfolio))]]
 
-    }
+        output_dict = {
+            '代码': [],
+            '名称': [],
+            '当前价格': [],
+            '周建议': [],
+            '日建议': [],
+            '均价偏移': [],
 
-    for symbol, name in tqdm(symbols, leave=False):
-        if pd.isna(name):
-            name = get_stock_name(symbol)
-        resp_weekly = pivot_points_grid(symbol, 'weekly', series)
-        df_weekly = resp_weekly['merged_table']
-        df_weekly.rename_axis('周内交易', inplace=True)
-        均价_weekly = df_weekly.loc['*均价>', series]
+        }
 
-        resp_daily = pivot_points_grid(symbol, 'daily', series)
-        df_daily = resp_daily['merged_table']
-        df_daily.rename_axis('日内交易', inplace=True)
-        均价_daily = df_daily.loc['*均价>', series]
+        for symbol, name in tqdm(symbols, leave=False):
+            if pd.isna(name):
+                name = get_stock_name(symbol)
+            resp_weekly = pivot_points_grid(symbol, 'weekly', series)
+            df_weekly = resp_weekly['merged_table']
+            df_weekly.rename_axis('周内交易', inplace=True)
+            均价_weekly = df_weekly.loc['*均价>', series]
 
-        output_dict["代码"].append(symbol)
-        output_dict["名称"].append(name)
-        output_dict["当前价格"].append(resp_weekly['price'])
-        output_dict["周建议"].append(format_for_term(resp_weekly['order']))
-        output_dict["日建议"].append(format_for_term(resp_daily['order']))
-        均价_diff = (均价_daily - 均价_weekly)/均价_weekly
-        output_dict["均价偏移"].append(f"{均价_diff:.2%}")
+            resp_daily = pivot_points_grid(symbol, 'daily', series)
+            df_daily = resp_daily['merged_table']
+            df_daily.rename_axis('日内交易', inplace=True)
+            均价_daily = df_daily.loc['*均价>', series]
 
-        if len(symbols) == 1:
-            weekly_table = tabulate(df_weekly,
-                                    headers='keys',
-                                    tablefmt="fancy_grid")
-            daily_table = tabulate(df_daily,
-                                   headers='keys',
-                                   tablefmt="fancy_grid")
+            output_dict["代码"].append(symbol)
+            output_dict["名称"].append(name)
+            output_dict["当前价格"].append(resp_weekly['price'])
+            output_dict["周建议"].append(format_for_term(resp_weekly['order']))
+            output_dict["日建议"].append(format_for_term(resp_daily['order']))
+            均价_diff = (均价_daily - 均价_weekly)/均价_weekly
+            output_dict["均价偏移"].append(f"{均价_diff:.2%}")
 
-            weekly_rows = weekly_table.split('\n')
-            daily_rows = daily_table.split('\n')
+            if len(symbols) == 1:
+                weekly_table = tabulate(df_weekly,
+                                        headers='keys',
+                                        tablefmt="fancy_grid")
+                daily_table = tabulate(df_daily,
+                                       headers='keys',
+                                       tablefmt="fancy_grid")
 
-            # 横向拼接行
+                weekly_rows = weekly_table.split('\n')
+                daily_rows = daily_table.split('\n')
 
-            combined_rows = [weekly + ' | ' + daily for weekly,
-                             daily in zip(weekly_rows, daily_rows)]
-            print('\n')
-            print('\n'.join(combined_rows))
+                # 横向拼接行
 
-    print(tabulate(pd.DataFrame(output_dict).set_index('代码'),
-                   headers=output_dict.keys(),
-                   tablefmt="fancy_grid"))
+                combined_rows = [weekly + ' | ' + daily for weekly,
+                                 daily in zip(weekly_rows, daily_rows)]
+                print('\n')
+                print('\n'.join(combined_rows))
+
+        print(tabulate(pd.DataFrame(output_dict).set_index('代码'),
+                       headers=output_dict.keys(),
+                       tablefmt="fancy_grid"))
+
+    @classmethod
+    def ai(cls, symbol: str, end_date: str = todaystr(), days_off: int = 30) -> None:
+        if symbol is None:
+            raise ValueError("symbol is required")
+
+        resp = ai_guide(
+            symbol=str(symbol),
+            end_date_str=str(end_date),
+            days_off=int(days_off))
+
+        print(resp)
 
 
 if __name__ == "__main__":
     start_time = time.time()
-    fire.Fire(guide)
+    fire.Fire(Guide)
     end_time = time.time()
     duration = end_time - start_time
     print(f"[{nowstr()} 执行完成, 花费:{duration:.2f}秒]")
