9810cbdc3c93ec56ebbee94a1e635bc8bd0b7d64
wizardbyron <1400427+wizardbyron@users.noreply.github.com>
2026-01-04 00:29:51 +0800

    更新投资组合名称，并且按代码顺序排列

--- update_names.py
+++ update_names.py
@@ -1,17 +1,11 @@
-import os
-
 import pandas as pd
 
 from src.data import get_stock_name
 
-path = "./input/portfolios"
-dir = os.listdir(path)
-
-for file in dir:
-    if file.endswith(".csv"):
-        full_path = os.path.join(path, file)
-        df = pd.read_csv(full_path, dtype={"代码": str, "名称": str})
-        for index, row in df.iterrows():
-            symbol = df.loc[index, "代码"]
-            df.loc[index, "名称"] = get_stock_name(symbol)
-        df.to_csv(full_path, index=False)
+full_path = 'input/portfolios/all.csv'
+df = pd.read_csv(full_path, dtype={"代码": str, "名称": str})
+for index, row in df.iterrows():
+    symbol = df.loc[index, "代码"]
+    df.loc[index, "名称"] = get_stock_name(symbol)
+df = df.sort_values('代码')
+df.to_csv(full_path, index=False)
